78beeffaaa38afa9b2bc48e4b37a92eeb615c72e
ElenaMoralesGil <elena.morales104@alu.ulpgc.es>
2025-02-13 17:26:46 +0000

    test: added test domain invoke should toggle subtask completion

---
+++ app/src/test/java/com/elena/autoplanner/domain/usecases/GetUseCasesTest.kt
@@ -0,0 +1,28 @@
+package com.elena.autoplanner.domain.usecases
+
+import com.elena.autoplanner.domain.models.Task
+import com.elena.autoplanner.domain.repository.TaskRepository
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+
+class GetUseCaseTest {
+    private val repository: TaskRepository = mockk()
+    private val useCase = GetTasksUseCase(repository)
+
+    @Test
+    fun `invoke should return flow of tasks`() = runTest {
+        val mockTasks = listOf(Task(id = 1, name = "Task 1"), Task(id = 2, name = "Task 2"))
+        every { repository.getTasks() } returns flowOf(mockTasks)
+
+        val resultFlow = useCase()
+
+        resultFlow.collect { tasks ->
+            assertEquals(2, tasks.size)
+        }
+    }
+}
