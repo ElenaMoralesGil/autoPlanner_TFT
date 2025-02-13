afb17ee34e24d79313505fb6dee57793a294ef7c
ElenaMoralesGil <elena.morales104@alu.ulpgc.es>
2025-02-13 16:54:35 +0000

    test: added test domain invoke should add subtask to existing task

---
+++ app/src/test/java/com/elena/autoplanner/domain/usecases/AddUseCasesTest.kt
@@ -0,0 +1,45 @@
+package com.elena.autoplanner.domain.usecases
+
+import com.elena.autoplanner.domain.models.Task
+import com.elena.autoplanner.domain.repository.TaskRepository
+import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.just
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class AddUseCasesTest {
+    private val repository: TaskRepository = mockk()
+    private val taskUseCase = AddTaskUseCase(repository)
+    private val subtaskUseCase = AddSubtaskUseCase(repository)
+
+    @Test
+    fun `invoke should save task via repository`() = runTest {
+        val testTask = Task(id = 1, name = "Test Task")
+        coEvery { repository.saveTask(testTask) } just Runs
+
+        taskUseCase(testTask)
+
+        coVerify(exactly = 1) { repository.saveTask(testTask) }
+    }
+
+    @Test
+    fun `invoke should add subtask to existing task`() = runTest {
+        val taskId = 1
+        val originalTask = Task(id = taskId, name = "Original Task", subtasks = emptyList())
+        coEvery { repository.getTask(taskId) } returns originalTask
+        coEvery { repository.saveTask(any()) } just Runs
+
+        val result = subtaskUseCase(taskId, "New Subtask")
+
+        assertEquals(1, result.subtasks.size)
+        assertEquals("New Subtask", result.subtasks.first().name)
+        coVerify { repository.saveTask(any()) }
+    }
+
+
+
+}
