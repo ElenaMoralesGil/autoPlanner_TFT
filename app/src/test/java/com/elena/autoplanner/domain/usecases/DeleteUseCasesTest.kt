eb99f629a7682797603043367fef44d4e3ae6b94
ElenaMoralesGil <elena.morales104@alu.ulpgc.es>
2025-02-13 17:20:55 +0000

    test: added test domain invoke should delete task via repository

---
+++ app/src/test/java/com/elena/autoplanner/domain/usecases/DeleteUseCasesTest.kt
@@ -0,0 +1,26 @@
+package com.elena.autoplanner.domain.usecases
+
+import com.elena.autoplanner.domain.models.Task
+import com.elena.autoplanner.domain.repository.TaskRepository
+import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.just
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class DeleteUseCaseTest {
+    private val repository: TaskRepository = mockk()
+    private val useCase = DeleteTaskUseCase(repository)
+
+    @Test
+    fun `invoke should delete task via repository`() = runTest {
+        val testTask = Task(id = 1, name = "Test Task")
+        coEvery { repository.deleteTask(testTask) } just Runs
+
+        useCase(testTask)
+
+        coVerify(exactly = 1) { repository.deleteTask(testTask) }
+    }
+}
