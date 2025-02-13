a9d320f5ce82c206af7526a9c0ef7c0d69ebf505
ElenaMoralesGil <elena.morales104@alu.ulpgc.es>
2025-02-13 17:44:32 +0000

    test: added test domain invoke should remove subtask from task and update task should also work

--- app/src/test/java/com/elena/autoplanner/domain/usecases/DeleteUseCasesTest.kt
+++ app/src/test/java/com/elena/autoplanner/domain/usecases/DeleteUseCasesTest.kt
@@ -1,5 +1,6 @@
 package com.elena.autoplanner.domain.usecases
 
+import com.elena.autoplanner.domain.models.Subtask
 import com.elena.autoplanner.domain.models.Task
 import com.elena.autoplanner.domain.repository.TaskRepository
 import io.mockk.Runs
@@ -8,19 +9,34 @@ import io.mockk.coVerify
 import io.mockk.just
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class DeleteUseCaseTest {
     private val repository: TaskRepository = mockk()
-    private val useCase = DeleteTaskUseCase(repository)
+    private val taskUseCase = DeleteTaskUseCase(repository)
+    private val subtaskUseCase = DeleteSubtaskUseCase(repository)
 
     @Test
     fun `invoke should delete task via repository`() = runTest {
         val testTask = Task(id = 1, name = "Test Task")
         coEvery { repository.deleteTask(testTask) } just Runs
 
-        useCase(testTask)
+        taskUseCase(testTask)
 
         coVerify(exactly = 1) { repository.deleteTask(testTask) }
+    }
+
+    @Test
+    fun `invoke should remove subtask from task`() = runTest {
+        val subtask = Subtask(id = 1, name = "To Delete")
+        val task = Task(id = 1, name = "Task", subtasks = listOf(subtask))
+        coEvery { repository.getTask(1) } returns task
+        coEvery { repository.saveTask(any()) } just Runs
+
+        val result = subtaskUseCase(1, 1)
+
+        assertEquals(0, result.subtasks.size)
+        coVerify { repository.saveTask(any()) }
     }
 }
